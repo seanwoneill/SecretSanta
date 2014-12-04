bf81b57d33942bb43be4c5b3bf9d21b92b9b7e44
S W ONeill <seanwoneill@gmail.com>
2014-12-03 20:36:53 -0500

    4th Commit

--- src/aa101/x601/secretsanta/MainActivity.java
+++ src/aa101/x601/secretsanta/MainActivity.java
@@ -1,6 +1,7 @@
 package aa101.x601.secretsanta;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Activity;
@@ -17,6 +18,7 @@ public class MainActivity extends Activity implements TextWatcher {
 	EditText numPpl;
 	List<String> names = Arrays.asList("Alice", "Bob", "Carol", "Dan", "Erin",
 			"Frank", "Gena", "Heather");
+	LinkedList<EditText> textWatcherNames;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,42 +30,56 @@ public class MainActivity extends Activity implements TextWatcher {
 	}
 
 	// @Override
-	public void onTextChanged() {
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
 		// NO-OP; Necessary to implement afterTextChanged()
 	}
 
 	// @Override
-	public void beforeTextChanged() {
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
 		// NO-OP; Necessary to implement afterTextChanged()
 	}
 
 	// @Override
-	public void afterTextChanged() {
+	public void afterTextChanged(Editable s) {
 
 		// Use try/catch to ensure numPpl input from user <= 20 and integer
 		// Force to 20 if > 20
-		Integer numPplInt = 0;
-		Integer numPplTry = 0;
+		int numPplTry = 0;
 		try {
-			numPplTry.parseInt(numPpl.getText().toString());
+			numPplTry = Integer.parseInt(numPpl.getText().toString());
 			if (numPplTry > 20) {
-				numPplInt = 20;
+				numPplTry = 20;
 				Toast.makeText(
 						getApplicationContext(),
 						"There cannot be more than 20 participants.\nNumber of participants has defaulted to 20.",
-						Toast.LENGTH_LONG);
+						Toast.LENGTH_LONG).show();
 			} else {
-				numPplInt = numPplTry.parseInt(numPpl.getText().toString());
+				numPplTry = Integer.parseInt(numPpl.getText().toString());
 			}
 		} catch (Exception e) {
 			Log.e(TAG, "Number of participants user entry: Not a valid number",
 					e);
 		}
 
-		// For loop to assign to each list element a necessary input field
-		for (int x = 0; x <= numPplInt; x++) {
-
+		/*
+		 * Use 'for' loop to: (1) Get integer address of EditText elements
+		 * stored in memory. (2) Use that address to store EditText pointers in
+		 * a linked list. (3) Add text changed listeners for each EditText
+		 * element stored in the linked list.
+		 */
+		for (int x = 0; x <= numPplTry; x++) {
+			String stringRId = "R.id." + names.get(x).toString();
+			int namePointer = Integer.parseInt(stringRId);
+			Log.d(TAG, "namePointer: " + namePointer + ": R.id.namePointer: "
+					+ namePointer);
+			textWatcherNames.add((EditText) findViewById(namePointer));
+			Log.d(TAG, "Most recent textWatcherNames linked list entry: "
+					+ textWatcherNames.get(x));
+			textWatcherNames.get(x).addTextChangedListener(this);
 		}
-
+		Log.d(TAG,
+				"textWatcherNames finel list length: "
+						+ textWatcherNames.size());
 	}
 }
